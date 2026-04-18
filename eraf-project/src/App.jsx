70bfe9ff0ed6d2fb453a9b1d4640392104115c7c
Omar2610sa <118198753+Omar2610sa@users.noreply.github.com>
2026-04-18 21:00:34 +0200

    feat: refactor App component to use AppRoutes for routing; create AppRoutes component for improved code organization

--- eraf-project/src/App.jsx
+++ eraf-project/src/App.jsx
@@ -1,48 +1,11 @@
-// Routes Pages 
-import { lazy, Suspense } from "react";
-const Home = lazy(() => import("./pages/Home/Home"))
-const About = lazy(() => import("./pages/About/About"));
-const Service = lazy(() => import("./pages/Services/Service"));
-const Blogs = lazy(() => import("./pages/Blogs/Blogs"));
-const BlogDetails = lazy(() => import("./pages/BlogDetails/BlogsDetails"));
-const ServiceDetails = lazy(() => import("./pages/ServiceDetails/ServiceDetails"));
-const ContactUs = lazy(() => import("./pages/ContactUs/ContactUs"));
-const ScrollTop = lazy(() => import("./components/ScrollTop/ScrollTop"));
-// Routs Hook
-import { Routes, Route } from "react-router-dom";
-
-const Departments = lazy(() => import("./pages/Departments/Departments"));
-const Terms = lazy(() => import("./pages/Terms/Terms"));
-const PrivacyPolicy = lazy(() => import("./pages/PrivacyPolicy/PrivacyPolicy.jsx"));
-const Error404 = lazy(() => import("./components/Error404/Error404"));
-const SeverNotFound = lazy(() => import("./SeverNotFound/SeverNotFound"));
-const JoinUs = lazy(() => import("./pages/JoinUs.jsx/JoinUs"));
-
-const Loading = lazy(() => import("./components/Loading/Loading"));
+import AppRoutes from "./routes/AppRoutes";
 
 const App = () => {
   return (
-    <div dir="rtl" className="bg-white" >
-      <ScrollTop />
-      <Suspense fallback={<Loading />}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/من-نحن" element={<About />} />
-          <Route path="/الخدمات" element={<Service />} />
-          <Route path="/الإدارات" element={<Departments />} />
-          <Route path="/الشروط-والأحكام" element={<Terms />} />
-          <Route path="/سياسة-الخصوصية" element={<PrivacyPolicy />} />
-          <Route path="/تواصل-معانا" element={<ContactUs />} />
-          <Route path="/404" element={<Error404 />} />
-          <Route path="/server-not-found" element={<SeverNotFound />} />
-          <Route path="/المدونة" element={<Blogs />} />
-          <Route path="/إنضم-إلينا" element={<JoinUs />} />
-          <Route path="/الخدمات/:id" element={<ServiceDetails />} />
-          <Route path="/blog/:id" element={<BlogDetails />} />
-        </Routes>
-      </Suspense>
+    <div dir="rtl">
+      <AppRoutes />
     </div>
-  )
- }
+  );
+}
 
- export default App
+export default App
