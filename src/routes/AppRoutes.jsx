682667d7a96b7cd1a1a3078b0b20447e93d10c5c
Omar2610sa <118198753+Omar2610sa@users.noreply.github.com>
2026-04-29 18:13:10 +0300

    's'

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -10,7 +10,7 @@ const ContactUs = lazy(() => import("../pages/ContactUs/ContactUs"));
 const ScrollTop = lazy(() => import("../components/ScrollTop/ScrollTop"));
 const Departments = lazy(() => import("../pages/Departments/Departments"));
 const Terms = lazy(() => import("../pages/Terms/Terms"));
-// const PrivacyPolicy = lazy(() => import("../pages/PrivacyPolicy/PrivacyPolicy.jsx"));
+const PrivacyPolicy = lazy(() => import("../pages/PrivacyPolicy/PrivacyPolicy.jsx"));
 const Error404 = lazy(() => import("../components/Error404/Error404"));
 const SeverNotFound = lazy(() => import("../SeverNotFound/SeverNotFound"));
 const JoinUs = lazy(() => import("../pages/JoinUs.jsx/JoinUs"));
